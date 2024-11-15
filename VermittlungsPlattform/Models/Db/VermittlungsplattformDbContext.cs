86343dad41924f8713c7bce1a62c40d2c2f64e1a
Fantomas077 <nheltonn@gmail.com>
2024-11-15 18:07:20 +0100

    AddDb Context and Table Menus

---
+++ VermittlungsPlattform/Models/Db/VermittlungsplattformDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace VermittlungsPlattform.Models.Db;
+
+public partial class VermittlungsplattformDbContext : DbContext
+{
+    public VermittlungsplattformDbContext()
+    {
+    }
+
+    public VermittlungsplattformDbContext(DbContextOptions<VermittlungsplattformDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Menu> Menus { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=FANTOMAS007\\SQLEXPRESS;Database=VermittlungsplattformDb;Trusted_Connection=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Menu>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Link)
+                .HasMaxLength(300)
+                .IsFixedLength();
+            entity.Property(e => e.MenuTitle)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Type)
+                .HasMaxLength(20)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
