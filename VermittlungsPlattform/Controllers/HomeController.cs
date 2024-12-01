2569d2dabafa9e1c7696d55d2cd59ea8ec567d89
Fantomas077 <nheltonn@gmail.com>
2024-12-01 01:15:17 +0100

    companydetails implementieren view

--- VermittlungsPlattform/Controllers/HomeController.cs
+++ VermittlungsPlattform/Controllers/HomeController.cs
@@ -1,22 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using VermittlungsPlattform.Models;
+using VermittlungsPlattform.Models.Db;
 
 namespace VermittlungsPlattform.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly VermittlungsplattformDbContext _context;
+       
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, VermittlungsplattformDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var Company = _context.UnternehmenProfiles.OrderByDescending(x => x.Id).Take(8).ToList();
+            ViewData["Company"] = Company;
+
+            var Stelle = _context.PraktikumStelles.ToList();
+            ViewData["Stelle"] = Stelle;
+
+
+            var Student = _context.Users.Where(x=>x.IsStudent==true).ToList();
+            ViewData["Student"] = Student;
+
             return View();
         }
+     
 
         public IActionResult Privacy()
         {
---
+++ VermittlungsPlattform/Controllers/UnternehmenController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VermittlungsPlattform.Models.Db;
+
+namespace VermittlungsPlattform.Controllers
+{
+
+    public class UnternehmenController : Controller
+    {
+        private readonly VermittlungsplattformDbContext _context;
+        public UnternehmenController( VermittlungsplattformDbContext context)
+        {
+            _context = context;
+            
+        }
+
+        public IActionResult Index()
+        {
+            List<UnternehmenProfile> obj = _context.UnternehmenProfiles.OrderByDescending(x => x.Id).ToList();
+            return View(obj);
+        }
+        public IActionResult Search(string Searchtext, string City)
+        {
+            var query = _context.UnternehmenProfiles.AsQueryable();
+
+            if (!string.IsNullOrEmpty(Searchtext))
+            {
+                query = query.Where(x => EF.Functions.Like(x.Name, "%" + Searchtext + "%")
+                                      || EF.Functions.Like(x.Branche, "%" + Searchtext + "%"));
+            }
+
+            if (!string.IsNullOrEmpty(City))
+            {
+                query = query.Where(x => EF.Functions.Like(x.Location, "%" + City + "%"));
+            }
+
+            var results = query.OrderBy(x => x.Name).ToList();
+
+
+            return View("Index", results);
+        }
+        public IActionResult CompanyDetails(int Id)
+        {
+            UnternehmenProfile? obj = _context.UnternehmenProfiles.FirstOrDefault(x => x.Id == Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            ViewData["Stelle"] = _context.PraktikumStelles.Where(x => x.UnternehmenProfileId == Id).ToList();
+            ViewData["gallery"] = _context.CompanyProfileGalleries.Where(x => x.CompanyProfileId == Id).ToList();
+            return View(obj);
+        }
+    }
+}
