0d39e0bec59d2c21a041ef1fd3e1e1026f203f5e
Fantomas077 <nheltonn@gmail.com>
2024-12-05 23:55:29 +0100

    StelleDetails und UnternehmenIndex implemementieren

--- VermittlungsPlattform/Controllers/StelleangeboteController.cs
+++ VermittlungsPlattform/Controllers/StelleangeboteController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using VermittlungsPlattform.Models.Db;
 
 namespace VermittlungsPlattform.Controllers
@@ -41,7 +42,49 @@ namespace VermittlungsPlattform.Controllers
             var company = _context.UnternehmenProfiles.ToList();
             ViewData["Company"] = company;
 
-            return View("Index", obj); // On réutilise la même vue "Index" pour afficher les résultats filtrés
+            return View("Index", obj); 
         }
+
+        public IActionResult Search(string Searchtext, string City)
+        {
+            // Commence par une requête de base incluant toutes les données
+            var query = _context.PraktikumStelles.AsQueryable();
+
+            // Applique le filtre sur le titre, les tags ou la branche si le Searchtext n'est pas vide
+            if (!string.IsNullOrEmpty(Searchtext))
+            {
+                query = query.Where(x =>
+                    EF.Functions.Like(x.Title, "%" + Searchtext + "%") ||
+                    EF.Functions.Like(x.Tags, "%" + Searchtext + "%") ||
+                    EF.Functions.Like(x.Branche, "%" + Searchtext + "%"));
+            }
+
+            // Applique le filtre sur la ville si la City n'est pas vide
+            if (!string.IsNullOrEmpty(City))
+            {
+                query = query.Where(x => EF.Functions.Like(x.Location, "%" + City + "%"));
+            }
+
+            // Trie les résultats par titre
+            var result = query.OrderBy(x => x.Title).ToList();
+            var company = _context.UnternehmenProfiles.ToList();
+            ViewData["Company"] = company;
+            // Renvoie la vue "Index" avec les résultats filtrés
+            return View("Index", result);
+        }
+
+        public IActionResult StelleDetails(int Id)
+        {
+           PraktikumStelle? obj = _context.PraktikumStelles.FirstOrDefault(x => x.Id == Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            var Company = _context.UnternehmenProfiles.ToList();
+            ViewData["Company"] = Company;
+            ViewData["Stelle"] = _context.PraktikumStelles.Where(x => x.UnternehmenProfileId == Id).ToList();
+            return View(obj);
+        }
+
     }
 }
--- VermittlungsPlattform/Controllers/UnternehmenController.cs
+++ VermittlungsPlattform/Controllers/UnternehmenController.cs
@@ -21,23 +21,31 @@ namespace VermittlungsPlattform.Controllers
         }
         public IActionResult Search(string Searchtext, string City)
         {
-            var query = _context.UnternehmenProfiles.AsQueryable();
+            // Commence par une requête de base incluant toutes les données
+            var query = _context.PraktikumStelles.AsQueryable();
+            var company = _context.UnternehmenProfiles.ToList();
+            ViewData["Company"] = company;
 
+            // Applique le filtre sur le titre, les tags ou la branche si le Searchtext n'est pas vide
             if (!string.IsNullOrEmpty(Searchtext))
             {
-                query = query.Where(x => EF.Functions.Like(x.Name, "%" + Searchtext + "%")
-                                      || EF.Functions.Like(x.Branche, "%" + Searchtext + "%"));
+                query = query.Where(x =>
+                    EF.Functions.Like(x.Title, "%" + Searchtext + "%") ||
+                    EF.Functions.Like(x.Tags, "%" + Searchtext + "%") ||
+                    EF.Functions.Like(x.Branche, "%" + Searchtext + "%"));
             }
 
+            // Applique le filtre sur la ville si la City n'est pas vide
             if (!string.IsNullOrEmpty(City))
             {
                 query = query.Where(x => EF.Functions.Like(x.Location, "%" + City + "%"));
             }
 
-            var results = query.OrderBy(x => x.Name).ToList();
-
-
-            return View("Index", results);
+            // Trie les résultats par titre
+            var result = query.OrderBy(x => x.Title).ToList();
+           
+            // Renvoie la vue "Index" avec les résultats filtrés
+            return View("Index", result);
         }
         public IActionResult CompanyDetails(int Id)
         {
